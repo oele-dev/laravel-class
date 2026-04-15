d3954aef6ca5b22912154321199ed4610d11440e
Osmell <correo.oele@gmail.com>
2026-04-15 00:27:08 +0000

    Redirect root URL to products route

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/products');
 
 Route::resource('categories', CategoryController::class);
 Route::resource('products', ProductController::class);
